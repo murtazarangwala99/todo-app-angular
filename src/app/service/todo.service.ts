0761d8a3f276fd733a4a3971f9abb554ccafe95c
murtazarangwala99 <murtazarangwala95@gmail.com>
2025-02-03 14:16:10 +0530

    Added functionality of browser localstorage

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -2,56 +2,72 @@ import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { Todo } from '../model/Todo';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  todos : Todo[];
+  todos: Todo[];
+
   constructor() { 
-    this.todos = [
+    const storedTodos = localStorage.getItem('todos');
+    this.todos = storedTodos ? JSON.parse(storedTodos) : [
       {
         id: '111',
         title: 'Learn C++',
         isCompleted: true,
-        date : new Date(),
+        date: new Date(),
       },
       {
         id: '222',
         title: 'Learn React',
         isCompleted: true,
-        date : new Date(),
+        date: new Date(),
       },
       {
         id: '333',
         title: 'Learn Angular',
         isCompleted: false,
-        date : new Date(),
-      }
-
+        date: new Date(),
+      },
+      {
+        id: '444',
+        title: 'Learn React',
+        isCompleted: false,
+        date: new Date(),
+      },
     ];
-
   }
 
-  getTodos(){
-    return of(this.todos)
+  private saveTodosToLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
-  addTodo(todo:Todo){
-    this.todos.push(todo)
+
+  getTodos() {
+    return of(this.todos);
   }
-  changeStatus(todo:Todo){
-    this.todos.map(singleTodo =>{
-      if(singleTodo.id === todo.id){
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+    this.saveTodosToLocalStorage();
+  }
+
+  changeStatus(todo: Todo) {
+    this.todos.map(singleTodo => {
+      if (singleTodo.id === todo.id) {
         todo.isCompleted = !todo.isCompleted;
       }
     });
+    this.saveTodosToLocalStorage();
   }
 
-
-  deleteTodo(todo:Todo){
+  deleteTodo(todo: Todo) {
     const indexofTodo = this.todos.findIndex(currentObj => currentObj.id === todo.id);
-    this.todos.splice(indexofTodo,1);
-
+    this.todos.splice(indexofTodo, 1);
+    this.saveTodosToLocalStorage();
   }
 
+  private loadTodosFromLocalStorage(): Todo[] {
+    const todos = localStorage.getItem('todos');
+    return todos ? JSON.parse(todos) : null;
+  }
 }
